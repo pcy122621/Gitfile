d9d33e8abdc4e2d2ee599b89d9e36d67a2370293
pcy <1544953294@qq.com>
2019-07-16 15:07:48 +0800

    second edit

--- log/src/pcy/main.java
+++ log/src/pcy/main.java
@@ -1,9 +1,10 @@
+package pcy;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-               System.out.println("≈”≥º—Ù");
+System.out.println("≈”≥º—Ù");
 	}
 
 }
