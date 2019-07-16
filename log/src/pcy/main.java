c6c658c4fd378c1315cac36cd62798e18d608946
epoch-chen <739447623@qq.com>
2019-07-16 17:32:09 +0800

    冲突修改4

--- log/src/pcy/main.java
+++ log/src/pcy/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("庞臣阳");
-               System.out.println("陈雁钊");
+                 System.out.println("陈雁钊");
 	}
 
 }
