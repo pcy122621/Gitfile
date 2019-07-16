aa69cbbc1e7507876e485a08b4f2abec998d3aec
YJC <1849409544@qq.com>
2019-07-16 16:37:57 +0800

    123

--- log/src/pcy/main.java
+++ log/src/pcy/main.java
@@ -1,11 +1,10 @@
-package pcy;
+﻿package pcy;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("厐臣阳");
-		System.out.println("杨杰成");
 	}
 
 }
